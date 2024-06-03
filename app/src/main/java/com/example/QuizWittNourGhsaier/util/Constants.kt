5b4ba02ab5ea7af2f21a88407a2694f9c40379a7
nour-ghsaier <148276631+nour-ghsaier@users.noreply.github.com>
2024-06-04 00:11:05 +0200

    Update Constants.kt

--- app/src/main/java/com/example/QuizWittNourGhsaier/util/Constants.kt
+++ app/src/main/java/com/example/QuizWittNourGhsaier/util/Constants.kt
@@ -1,9 +1,6 @@
 package com.example.QuizWittNourGhsaier.util
 
 object Constants {
-    const val BASE_URL = "https://raw.githubusercontent.com/ClandEstine47/English-Quiz-App/master/"
+    const val BASE_URL = "https://raw.githubusercontent.com/nour-ghsaier/QuizWittNourGhsaier/master/"
 
-
-   // https://raw.githubusercontent.com/nour-ghsaier/QuizWittNourGhsaier/master/
-   //  https://raw.githubusercontent.com/nour-ghsaier/QuizWittNourGhsaier/master/quizList.json?token=GHSAT0AAAAAACRZHY7SOYENWCGLGJ7DXRUUZS4TIBQ
 }
